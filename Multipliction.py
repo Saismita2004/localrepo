5b55ef72a1214e1d595014fbee8833ca40a50399
Saismita Mohanty <saismitamohanty20211@gmail.com>
2025-09-03 23:05:27 +0530

    Add Initial Files

---
+++ Multipliction.py
@@ -0,0 +1,75 @@
+saism  C:\Users\saism
+  .config
+  .continuum
+  .dotnet
+  .eclipse
+  .idlerc
+  .ipynb_checkpoints
+  .ipython
+  .jupyter
+  .p2
+    org.eclipse.equinox.p2.core
+    org.eclipse.equinox.p2.engine
+    org.eclipse.equinox.p2.repository
+    pool
+    pools.info
+    profiles.info
+  .templateengine
+  .vscode
+  anaconda3
+  AppData
+  Application Data
+  cfiles
+  classroom
+  Contacts
+  Cookies
+  csapp
+  Desktop
+  Documents
+  Downloads
+  eclipse
+  Favorites
+  index.js M
+  IntelGraphicsProfiles
+  java
+  Links
+  Local Settings
+  Music
+  My Documents
+  mysql
+  NetHood
+  node_modules
+  OneDrive
+  Postman
+  PrintHood
+  PycharmProjects
+  Recent
+  requir_placeholder.html
+  Saved Games
+  Searches
+  SendTo
+  Start Menu
+  Templates
+  Videos
+  .bash_history
+  .gitconfig
+  .gitignore
+  .node_repl_history
+  dropdown.html
+  eat.html
+  exersie5soluation.html
+  inheritance.py
+  NTUSER.DAT
+  ntuser.dat.LOG1
+  ntuser.dat.LOG2
+  NTUSER.DAT{5c8033b9-c94a-11ed-963d-9cda3ed44856}.TM.blf
+  NTUSER.DAT{5c8033b9-c94a-11ed-963d-9cda3ed44856}.TMContainer00000000000000000001.regtrans-ms
+  NTUSER.DAT{5c8033b9-c94a-11ed-963d-9cda3ed44856}.TMContainer00000000000000000002.regtrans-ms
+  ntuser.ini
+  package.json
+  package-lock.json
+  requirment_placeholder.html
+  thankyou.html
+  Untitled.ipynb
+External Libraries
+Scratches and Consoles
